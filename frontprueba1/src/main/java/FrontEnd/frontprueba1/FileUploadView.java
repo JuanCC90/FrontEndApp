479202eb03a84eb964af2850b863d24813d1f2a7
Admin <Admin@192.168.124.148>
2019-04-03 11:36:59 +0200

    Intentando añadir archivo asociado al objeto

---
+++ frontprueba1/src/main/java/FrontEnd/frontprueba1/FileUploadView.java
@@ -0,0 +1,36 @@
+package FrontEnd.frontprueba1;
+
+import javax.annotation.ManagedBean;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+import org.primefaces.event.FileUploadEvent;
+import org.primefaces.model.UploadedFile;
+
+@ManagedBean
+public class FileUploadView {
+
+	private UploadedFile file;
+	
+	public UploadedFile getFile() {
+		return file;
+	}
+	
+	public void setFile(UploadedFile file) {
+		this.file=file;
+	}
+	
+	public void upload() {
+		if(file != null) {
+			FacesMessage message = new FacesMessage(file.getFileName()+" Archivo Subido Correctamente");
+			FacesContext.getCurrentInstance().addMessage(null, message);
+		}
+	}
+	
+	public void handleFileUpload(FileUploadEvent event) {
+		FacesMessage msg = new FacesMessage(event.getFile().getFileName()+" Archivo Subido Correctamente");
+		FacesContext.getCurrentInstance().addMessage(null, msg);
+	}
+	
+	
+}
